898e900c0d39c4ac863d025fe57bf18570ec587d
chenshenhai <cshenhai@gmail.com>
2017-02-02 22:26:45 +0800

    set upload picture assort

--- server/controllers/picture.js
+++ server/controllers/picture.js
@@ -3,61 +3,12 @@ const path = require('path')
 const os = require('os')
 const fs = require('fs')
 const Busboy = require('busboy')
-
-
-function uploadEvent( ctx ) {
-  let req = ctx.req
-  let res = ctx.res
-
-  let busboy = new Busboy({headers: req.headers})
-
-  return new Promise((resolve, reject) => {
-    let result = { 
-      success: false,
-      code: '',
-      message: '',
-      data: null
-    }
-
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      console.log('File-file [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype)
-      let _uploadFilePath = path.join(__dirname, './../../static/output/upload/' + new Date().getTime() + '.jpg' )
-      console.log(_uploadFilePath)
-      
-      let saveTo = path.join(_uploadFilePath)
-      file.pipe(fs.createWriteStream(saveTo))
-
-      file.on('data', function(data) {
-        console.log('File-data [' + fieldname + '] got ' + data.length + ' bytes')
-      })
-      file.on('end', function() {
-        console.log('File-end [' + fieldname + '] Finished')
-      })
-    })
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('Field-field [' + fieldname + ']: value: ' + inspect(val))
-    })
-    busboy.on('finish', function() {
-      console.log('Done parsing form!')
-      result.success = true
-      resolve(result)
-    })
-
-    busboy.on('error', function(err) {
-      console.log('File-error')
-      reject(result)
-    })
-
-    req.pipe(busboy)
-  })
-
-    
-} 
+const { uploadPicture } = require('./../utils/upload')
 
 const pictureController = { 
 
   async upload( ctx ) {
-    let result = await uploadEvent( ctx )
+    let result = await uploadPicture( ctx, { pictureType: 'album' } )
     ctx.body = result
   },
 
--- server/utils/datetime.js
+++ server/utils/datetime.js
@@ -21,11 +21,35 @@ const timeEnum = [
 
 const datatime = {
 
-  parseStampToFormat( timestamp ) {
-    let _date = new Date(timestamp);
-    let parsedDate = `${_date.getFullYear()}-${monthEnum[_date.getMonth()]}-${dayEnum[_date.getDate()-1]}`;
-    let parseTime = `${timeEnum[_date.getHours()]}:${timeEnum[_date.getMinutes()]}:${timeEnum[_date.getSeconds()]}`;
-    let parseDatetime = `${parsedDate} ${parseTime}`;
+  parseStampToFormat( timestamp, type ) {
+    let _date
+    if ( timestamp ) {
+      _date = new Date(timestamp)
+    } else {
+      _date = new Date()
+    }
+    
+    let parsedDate
+    let parseTime 
+    let parseDatetime
+
+    let yearNum = _date.getFullYear()
+    let monthNum = monthEnum[_date.getMonth()]
+    let dayNum = dayEnum[_date.getDate()-1]
+    let hourNum = timeEnum[_date.getHours()]
+    let minNum = timeEnum[_date.getMinutes()]
+    let secNum = timeEnum[_date.getSeconds()]
+
+    type = type || 'YYYY/MM/DD/hh/mm/ss'
+
+    parseDatetime = type
+      .replace('YYYY', yearNum)
+      .replace('MM', monthNum)
+      .replace('DD', dayNum)
+      .replace('hh', hourNum)
+      .replace('mm', minNum)
+      .replace('ss', secNum)
+    
     return parseDatetime;
   },
 
@@ -33,7 +57,9 @@ const datatime = {
     let timestamp = new Date().getTime()
     let nowDatetime = this.parseStampToFormat( timestamp )
     return nowDatetime
-  }
+  },
+
+
 
 };
 
