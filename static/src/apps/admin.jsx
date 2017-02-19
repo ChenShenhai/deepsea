c56072b9b01b2508b4ecc8a4b1427feb62d0ad87
chenshenhai <cshenhai@gmail.com>
2017-02-19 10:58:37 +0800

    add antd

--- static/src/apps/admin.jsx
+++ static/src/apps/admin.jsx
@@ -1,232 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import validator from 'validator';
-import TagController from './../components/tab-controller.jsx';
-import Alert from './../components/alert.jsx';
-import Tools from './../utils/tool';
-import Request from './../utils/request';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import FormGroup from './../components/form-group.jsx'
+
+import 'antd/lib/layout/style/css'
+
+const { Header, Content, Footer } = Layout
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-
-
-    let isSignUpSuccess = Tools.getUrlParam('signUpSuccess');
-    let signInAlertType = '';
-    let signInAlertContext = '';
-    if ( isSignUpSuccess + '' === 'true' ) {
-      signInAlertType = 'info';
-      signInAlertContext = '注册成功！请登录！';
-    }
-
-    console.log( signInAlertType, signInAlertContext )
-
-    this.state = {
-      signUpAlert: {
-        type: '',
-        context: '',
-      },
-      signInAlert: {
-        type: signInAlertType,
-        context: signInAlertContext,
-      }
-    };
-  }
-
-  handleSignIn() {
-    let signInfo = this.getSignInUserInfo();
-    Request.post({
-      url: '/api/user/signIn.json',
-      data: signInfo,
-      success: ( result ) => {
-        console.log( result );
-        if ( result && result.success === true ) {
-          signInfo.source = 'form';
-          Request.form({
-            url: '/api/user/signIn.json',
-            data: signInfo,
-          })
-        } else {
-          this.setState({
-            signInAlert: {
-              type: 'danger',
-              context: result.message || '',
-            }
-          });
-        }
-      },
-      error: ( err ) => {
-        console.log( err );
-      }
-    })
-  }
-
-  handleSignUp() {
-    let userInfo = this.getSignUpUserInfo();
-    let validateResult = this.validatorSignUp( userInfo );
-
-    if ( validateResult.success === false ) {
-      this.setState({
-        signUpAlert: {
-          type: 'danger',
-          context: validateResult.message || '',
-        }
-      });
-      return false;
-    } else {
-      this.setState({
-        signUpAlert: {
-          type: 'info',
-          context: '',
-        }
-      })
-    }
-
-    let _this = this;
-
-    Request.post({
-      url: '/api/user/signUp.json',
-      data: userInfo,
-      success: ( result ) => {
-        if ( result && result.success === true  ) {
-          window.location.href = '/admin?signUpSuccess=true'
-        } else {
-          _this.setState({ signUpAlert: {
-            type: 'danger',
-            context: result.message,
-          }})
-        }
-      },
-      error: ( err ) => {
-        console.log( err )
-        _this.setState({ signUpAlert: {
-          type: 'danger',
-          context: '系统繁忙！',
-        }})
-      }
-    })
-
-  }
-
-  getSignInUserInfo() {
-    let userName = ReactDOM.findDOMNode(this.refs.signInUserName).value.trim();
-    let password = ReactDOM.findDOMNode(this.refs.signInPassword).value.trim();
-
-    return {
-      userName,
-      password,
-    }
-  }
-
-  getSignUpUserInfo() {
-    let userName = ReactDOM.findDOMNode(this.refs.signUpUserName).value.trim();
-    let email = ReactDOM.findDOMNode(this.refs.signUpEmail).value.trim();
-    let password = ReactDOM.findDOMNode(this.refs.signUpPassword).value.trim();
-    let confirmPassword = ReactDOM.findDOMNode(this.refs.signUpConfirmPassword).value.trim();
-
-    return {
-      userName,
-      email,
-      password,
-      confirmPassword
-    }
-  }
-
-  validatorSignUp( userInfo ) {
-    let result = {
-      success: false,
-      message: '',
-    }
-
-    if ( /[a-z0-9\_\-]{6,16}/.test(userInfo.userName) === false ) {
-      result.message = '用户名格式为6-16位的小写字母，包括-、_';
-      return result;
-    }
-    if ( !validator.isEmail( userInfo.email ) ) {
-      result.message = '请输入正确的邮箱地址';
-      return result;
-    }
-    if ( !/[\w+]{6,16}/.test( userInfo.password )  ) {
-      result.message = '密码长度应该为6-16';
-      return result;
-    }
-    if ( userInfo.password !== userInfo.confirmPassword ) {
-      result.message = '两次密码不一致';
-      return result;
-    }
-
-    result.success = true;
-
-    return result
-  }
-
-
-  render () {
-
+  render() {
     return (
-      <div className="container">
-
-        <div className="form-sign">
-          <TagController data-active-index="1">
-
-            <div name="登录">
-              <div className="form-sign-content">
-                <Alert
-                  alertType={this.state.signInAlert.type}
-                  alertContext={this.state.signInAlert.context} >
-                </Alert>
-                <label>登录名</label>
-                <input
-                  ref="signInUserName"
-                  type="text"  className="form-control" placeholder="请输入登录名" />
-                <label className="">密码</label>
-                <input
-                  ref="signInPassword"
-                  type="password"  className="form-control" placeholder="请输入密码" />
-
-                <button
-                  onClick={this.handleSignIn.bind(this)}
-                  className="btn btn-lg btn-primary btn-block" type="submit">登录</button>
-              </div>
-            </div>
-
-            <div name="注册">
-              <div className="form-sign-content">
-                <Alert
-                  alertType={this.state.signUpAlert.type}
-                  alertContext={this.state.signUpAlert.context} >
-                </Alert>
-
-                <label>注册名</label>
-                <input
-                  ref="signUpUserName"
-                  type="text" className="form-control" placeholder="请输入登录名" />
-                <label>注册邮箱</label>
-                <input
-                  ref="signUpEmail"
-                  type="text" className="form-control" placeholder="请输入邮箱地址" />
-                <label>密码</label>
-                <input
-                  ref="signUpPassword"
-                  type="password" className="form-control" placeholder="请输入密码" />
-                <label>确认密码</label>
-                <input
-                  ref="signUpConfirmPassword"
-                  type="password" className="form-control" placeholder="确认密码" />
-                <button
-                  onClick={this.handleSignUp.bind(this)}
-                  className="btn btn-lg btn-primary btn-block" >注册</button>
-              </div>
-            </div>
-
-          </TagController>
-
-        </div>
-
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '12px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <FormGroup />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2016 Created by Ant UED
+        </Footer>
+      </Layout>
     )
   }
 }
 
-export default App;
+
+export default App
---
+++ static/src/components/sign-up-form.jsx
@@ -0,0 +1,165 @@
+import React from 'react'
+import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button } from 'antd';
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+const residences = [{
+  value: 'zhejiang',
+  label: 'Zhejiang',
+  children: [{
+    value: 'hangzhou',
+    label: 'Hangzhou',
+    children: [{
+      value: 'xihu',
+      label: 'West Lake',
+    }],
+  }],
+}, {
+  value: 'jiangsu',
+  label: 'Jiangsu',
+  children: [{
+    value: 'nanjing',
+    label: 'Nanjing',
+    children: [{
+      value: 'zhonghuamen',
+      label: 'Zhong Hua Men',
+    }],
+  }],
+}];
+
+const SignUpForm = Form.create()(React.createClass({
+  getInitialState() {
+    return {
+      passwordDirty: false,
+    };
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  },
+  handlePasswordBlur(e) {
+    const value = e.target.value;
+    this.setState({ passwordDirty: this.state.passwordDirty || !!value });
+  },
+  checkPassword(rule, value, callback) {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  },
+  checkConfirm(rule, value, callback) {
+    const form = this.props.form;
+    if (value && this.state.passwordDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  },
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        span: 14,
+        offset: 6,
+      },
+    };
+    const prefixSelector = getFieldDecorator('prefix', {
+      initialValue: '86',
+    })(
+      <Select className="icp-selector">
+        <Option value="86">+86</Option>
+      </Select>
+    );
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormItem
+          {...formItemLayout}
+          label={(
+            <span>
+              UserName&nbsp;
+              <Tooltip title="What do you want other to call you?">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+          )}
+          hasFeedback
+        >
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="E-mail"
+          hasFeedback
+        >
+          {getFieldDecorator('email', {
+            rules: [{
+              type: 'email', message: 'The input is not valid E-mail!',
+            }, {
+              required: true, message: 'Please input your E-mail!',
+            }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Password"
+          hasFeedback
+        >
+          {getFieldDecorator('password', {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            }, {
+              validator: this.checkConfirm,
+            }],
+          })(
+            <Input type="password" onBlur={this.handlePasswordBlur} />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Confirm Password"
+          hasFeedback
+        >
+          {getFieldDecorator('confirm', {
+            rules: [{
+              required: true, message: 'Please confirm your password!',
+            }, {
+              validator: this.checkPassword,
+            }],
+          })(
+            <Input type="password" />
+          )}
+        </FormItem>
+        
+        <FormItem {...tailFormItemLayout} style={{ marginBottom: 8 }}>
+          {getFieldDecorator('agreement', {
+            valuePropName: 'checked',
+          })(
+            <Checkbox>I had read the <a>agreement</a></Checkbox>
+          )}
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit" size="large">Register</Button>
+        </FormItem>
+      </Form>
+    );
+  },
+}));
+
+
+export default SignUpForm
+
