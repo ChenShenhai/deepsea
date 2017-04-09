4f05921ccc430354261e5395eca457a100374d3a
chenshenhai <cshenhai@gmail.com>
2017-04-09 23:15:37 +0800

    feat: edit sql

--- init/sql/user_info.sql
+++ init/sql/user_info.sql
@@ -1,5 +1,5 @@
 CREATE TABLE   IF NOT EXISTS  `user_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT, 
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -7,8 +7,8 @@ CREATE TABLE   IF NOT EXISTS  `user_info` (
   `detail_info` json DEFAULT NULL,
   `create_time` varchar(20) DEFAULT NULL,
   `update_time` varchar(20) DEFAULT NULL,
-  `level` int(11) DEFAULT 1,
-  `status` int(11) DEFAULT 1,
+  `level` int(11) DEFAULT 1, -- user level, 0:super admin, 1:common admin, 2:common user
+  `status` int(11) DEFAULT 1, -- user status, 0:super admin, 1:common, 2:forbidden, 3:delete
   PRIMARY KEY (`id`),
   UNIQUE KEY (`email`),
   UNIQUE KEY (`name`)
@@ -18,8 +18,3 @@ INSERT INTO
 `user_info`
  (`id`,`name`,`email`,`password`,`detail_info`,`create_time`,`update_time`,`level`,`status`) 
  VALUES (DEFAULT,'admin01','01@admin.com','098765','{}',0,0,0,0);
-
---  INSERT INTO 
--- `user_info`
---  (`id`,`name`,`email`,`password`,`detail_info`,`create_time`,`update_time`,`level`,`status`) 
---  VALUES (DEFAULT,'admin01','01@admin.com','098765','{}',0,0,0,0);
