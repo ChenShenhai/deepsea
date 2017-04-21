8fe99ba3797d05037e7409ab497335532c561fe0
chenshenhai <cshenhai@gmail.com>
2017-04-22 00:34:13 +0800

    feat: update

--- static/src/views/my-info/index.jsx
+++ static/src/views/my-info/index.jsx
@@ -1,9 +1,7 @@
 
 import React from 'react';
 import Request from '@@utils/request';
-import { Form, Select, Input, Button } from 'antd';
-import './style.less'
-// import 'antd/lib/layout/style/css'
+import { Form, Select, Input, Button, Row, Col } from 'antd'; 
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -30,7 +28,7 @@ class App extends React.Component {
         <FormItem
           label="Note"
           labelCol={{ span: 4 }}
-          wrapperCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
         >
           {getFieldDecorator('note', {
             rules: [{ required: true, message: 'Please input your note!' }],
@@ -41,7 +39,7 @@ class App extends React.Component {
         <FormItem
           label="Gender"
           labelCol={{ span: 4 }}
-          wrapperCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
         >
           {getFieldDecorator('gender', {
             rules: [{ required: true, message: 'Please select your gender!' }],
@@ -93,8 +91,13 @@ class View extends React.Component {
   render () {
     return (
       <div>
-        <h1 className="hello">my-info</h1>
-        <WrappedApp />
+        <Row>
+          <Col span={12}>
+            <WrappedApp />
+          </Col>
+          <Col span={12}>col-12</Col>
+        </Row>
+        
       </div>
     )
   }
