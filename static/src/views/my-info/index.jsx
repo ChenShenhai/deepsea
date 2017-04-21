819b8678941e123943ba4f1724b8b7135f8fbb36
chenshenhai <cshenhai@gmail.com>
2017-04-22 01:04:55 +0800

    feat: update

--- static/src/views/my-info/index.jsx
+++ static/src/views/my-info/index.jsx
@@ -9,29 +9,20 @@ const Option = Select.Option;
 class App extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
+    
   }
-  handleSelectChange = (value) => {
-    console.log(value);
-    this.props.form.setFieldsValue({
-      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
-    });
-  }
+  
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit}>
         <FormItem
-          label="Note"
+          label="username"
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
         >
-          {getFieldDecorator('note', {
-            rules: [{ required: true, message: 'Please input your note!' }],
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
           })(
             <Input />
           )}
@@ -43,7 +34,6 @@ class App extends React.Component {
         >
           {getFieldDecorator('gender', {
             rules: [{ required: true, message: 'Please select your gender!' }],
-            onChange: this.handleSelectChange,
           })(
             <Select placeholder="Select a option and change input text above">
               <Option value="male">male</Option>
