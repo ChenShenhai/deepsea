ba43a7dfa89c93cae884d65bfc705f9141729fca
chenshenhai <cshenhai@gmail.com>
2017-04-25 00:48:44 +0800

    feat: edit form method

--- static/src/views/my-info/info.jsx
+++ static/src/views/my-info/info.jsx
@@ -2,39 +2,67 @@ import React from 'react';
 
 import Form from 'antd/lib/form';
 import Input from 'antd/lib/input'; 
+import Button from 'antd/lib/button'; 
 
 import Texts from '@@texts/index';
 
 const FormItem = Form.Item;
+const formItemLayout = {
+  labelCol: {
+    span: 4
+  },
+  wrapperCol: {
+    span: 8
+  },
+};
 
 const CustomizedForm = Form.create({
+  
   onFieldsChange(props, changedFields) {
     props.onChange(changedFields);
   },
+
   mapPropsToFields(props) {
+    
     return {
       name: {
         ...props.name,
-        value: props.name.value.toUpperCase(),
+        value: props.name.value,
+      },
+      nick: {
+        ...props.nick,
+        value: props.nick.value,
       },
     };
   },
+
   onValuesChange(_, values) {
     console.log(values);
   },
 })((props) => {
   const { getFieldDecorator } = props.form;
   return (
-    <Form layout="inline">
-      <FormItem label={Texts.view.LABEL_USER_NAME}>
+    <Form onSubmit={this.handleSubmit}>
+      <FormItem 
+        {...formItemLayout}
+        label={Texts.view.LABEL_USER_NAME}>
         {getFieldDecorator('name', {
           rules: [{ required: true, message: 'Username is required!' }],
         })(<Input />)}
       </FormItem>
-       <FormItem label={Texts.view.LABEL_NICK}>
+      <FormItem 
+        {...formItemLayout}
+        label={Texts.view.LABEL_NICK}>
         {getFieldDecorator('nick', {
           rules: [{ required: true, message: 'Username is required!' }],
         })(<Input />)}
+      </FormItem>
+      <FormItem
+        wrapperCol={{ span: 8, offset: 4 }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </FormItem>
     </Form>
   );
@@ -51,19 +79,29 @@ class Info extends React.Component {
         value: '',
       },
     },
+    isInitedFields: false,
   };
+
+  componentDidUpdate() {
+    let {userInfo} = this.props;
+    let {isInitedFields} = this.state;
+    if ( isInitedFields === false ) {
+      let fields = this.getFields(userInfo);
+      this.setState({
+        fields,
+        isInitedFields: true,
+      })
+      // console.log( 'componentWillReceiveProps=', userInfo );
+    }
+  }
   
   handleFormChange = (changedFields) => {
-    // const fields = this.state.fields;
-    // let { userInfo } = this.props;
-    // let fileds = this.getFileds( userInfo )
-    // this.setState({
-    //   fields: { ...fields, ...changedFields },
-    // });
-    // 
+    this.setState({
+      fields: { ...this.state.fields, ...changedFields },
+    });
   }
 
-  getFileds( userInfo ) {
+  getFields( userInfo ) {
     return {
       name: {
         value: userInfo.name,
@@ -80,10 +118,21 @@ class Info extends React.Component {
     }
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   render() {
     let fields = this.state.fields;
+    
+    // let fileds = this.getFileds(userInfo);
     return (
-      <div>
+      <div onSubmit={(e) => {this.handleSubmit(e)}}>
         <CustomizedForm {...fields} onChange={this.handleFormChange} />
         <pre className="language-bash">
           {JSON.stringify(fields, null, 2)}
